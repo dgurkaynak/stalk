d07d59bc9c095b3898210033111576adf8bb9c40
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-06-03 22:25:17 +0300

    Allow CORS by disabling electron web security

--- scripts/electron-main.js
+++ scripts/electron-main.js
@@ -11,7 +11,13 @@ let mainWindow;
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      webSecurity: false
+    }
+  });
 
   // and load the index.html of the app.
   const startUrl =
