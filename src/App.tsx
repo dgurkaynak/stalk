2f9c9dacd113617859c5e195174fffba34da1bae
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-06-04 10:32:53 +0300

    Sidebar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,49 @@
 import React from 'react';
-import { Button } from 'antd';
-import logo from './logo.svg';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import './App.css';
+
+const { Content, Footer, Sider } = Layout;
+
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          <Button type="primary">Button</Button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+    <Sider className="app-sider" trigger={null} collapsible collapsed={true}>
+      <div className="logo" />
+      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+        <Menu.Item key="1">
+          <Icon type="database" />
+          <span>Data Sources</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <Icon type="search" />
+          <span>Search</span>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <Icon type="line-chart" />
+          <span>Timeline View</span>
+        </Menu.Item>
+        <Menu.Item key="4">
+          <Icon type="setting" />
+          <span>Settings</span>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout>
+      {/* <Header style={{ background: '#fff', padding: 0 }} /> */}
+      <Content style={{ margin: '0 16px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>User</Breadcrumb.Item>
+          <Breadcrumb.Item>Bill</Breadcrumb.Item>
+        </Breadcrumb>
+        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
   );
 }
+
 
 export default App;
