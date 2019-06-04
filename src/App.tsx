ae59eadd410f4a0488bf97d9d7c7f0b264307628
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-06-04 10:49:30 +0300

    Router groundwork

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,57 @@
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 const { Content, Footer, Sider } = Layout;
 
-
+const Datasources: React.FC = () => <div>data sources</div>;
+const Search: React.FC = () => <div>search</div>;
+const Timeline: React.FC = () => <div>timeline</div>;
+const Settings: React.FC = () => <div>settings</div>;
 
 const App: React.FC = () => {
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-    <Sider className="app-sider" trigger={null} collapsible collapsed={true}>
-      <div className="logo" />
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-        <Menu.Item key="1">
-          <Icon type="database" />
-          <span>Data Sources</span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="search" />
-          <span>Search</span>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="line-chart" />
-          <span>Timeline View</span>
-        </Menu.Item>
-        <Menu.Item key="4">
-          <Icon type="setting" />
-          <span>Settings</span>
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout>
-      {/* <Header style={{ background: '#fff', padding: 0 }} /> */}
-      <Content style={{ margin: '0 16px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>User</Breadcrumb.Item>
-          <Breadcrumb.Item>Bill</Breadcrumb.Item>
-        </Breadcrumb>
-        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-    </Layout>
-  </Layout>
+    <Router>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider className="app-sider" trigger={null} collapsible collapsed={true}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="database" />
+              <span>Data Sources</span>
+              <Link to="/datasources" />
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="search" />
+              <span>Search</span>
+              <Link to="/search" />
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="line-chart" />
+              <span>Timeline View</span>
+              <Link to="/timeline" />
+            </Menu.Item>
+            <Menu.Item key="4">
+              <Icon type="setting" />
+              <span>Settings</span>
+              <Link to="/settings" />
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          {/* <Header style={{ background: '#fff', padding: 0 }} /> */}
+          <Content style={{ margin: '0 16px' }}>
+            <Route exact path="/" component={Datasources} />
+            <Route path="/datasources" component={Datasources} />
+            <Route path="/search" component={Search} />
+            <Route path="/timeline" component={Timeline} />
+            <Route path="/settings" component={Settings} />
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    </Router>
   );
 }
 
