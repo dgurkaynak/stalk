0bfece4b11274014f4bbdc6e5c3a12238f74311f
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-10-08 14:57:35 +0300

    Init datasource maneger before react

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import DatasourceManager from './model/datasource/manager';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+async function main() {
+    const datasourceManager = DatasourceManager.getSingleton();
+    await datasourceManager.init();
+
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
+main().catch(err => console.error(err))
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
