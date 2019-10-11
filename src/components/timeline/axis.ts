e4509d912dacaf269bfd4c886c38791c2ef84e9d
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-10-12 01:40:30 +0300

    timeline: Limit horizontal panning in timeline bounds

--- src/components/timeline/axis.ts
+++ src/components/timeline/axis.ts
@@ -2,6 +2,7 @@
 export default class Axis {
   private scale: number;
   private offset: number;
+  private minScale: number;
 
   constructor(
     private inputRange: [number, number],
@@ -9,6 +10,7 @@ export default class Axis {
   ) {
     this.scale = (outputRange[1] - outputRange[0]) / (inputRange[1] - inputRange[0]);
     this.offset = outputRange[0] - (this.scale * inputRange[0]);
+    this.minScale = this.scale;
   }
 
   input2output(x: number) {
@@ -20,11 +22,29 @@ export default class Axis {
   }
 
   zoom(factor: number, anchorOutputPoint: number) {
-    this.scale *= factor;
+    const newScale = Math.max(this.scale * factor, this.minScale);
+    if (newScale === this.scale) return;
+    this.scale = newScale;
     this.offset = anchorOutputPoint * (1 - factor) + (this.offset * factor);
+    this.preventTranslatingOutOfRange();
   }
 
   translate(delta: number) {
     this.offset += delta;
+    this.preventTranslatingOutOfRange();
+  }
+
+  preventTranslatingOutOfRange() {
+    const minInput = this.inputRange[0];
+    const minOutput = this.input2output(minInput);
+    if (minOutput > this.outputRange[0]) {
+      this.offset += this.outputRange[0] - minOutput;
+    }
+
+    const maxInput = this.inputRange[1];
+    const maxOutput = this.input2output(maxInput);
+    if (maxOutput < this.outputRange[1]) {
+      this.offset += this.outputRange[1] - maxOutput;
+    }
   }
 }
