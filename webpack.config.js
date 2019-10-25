885de953ea903875bd66d01af12ef27151a56142
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-10-25 03:19:23 +0300

    chore(webpack): Enable caching with hard-source-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 
 module.exports = {
@@ -38,6 +39,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new HardSourceWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Stalk Studio',
       template: path.resolve(__dirname, 'src/index.html'),
