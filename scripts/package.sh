3db2f1e079b1b63cfe93fa480371d4671cbed0da
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-11-19 11:05:43 +0300

    chore(package): Ignore scripts folder

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,1 +1,1 @@
-electron-packager . --ignore=node_modules/.cache --ignore=src --ignore="build/.*\.js\.map"
+electron-packager . --ignore=node_modules/.cache --ignore=src --ignore="build/.*\.js\.map" --ignore=scripts
