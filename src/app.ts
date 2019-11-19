a723f8676c9618bfcdb22053dc9c4c34eabcf028
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-11-19 15:29:19 +0300

    feat(app): Split layout groundwork

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,28 @@
+import Split from 'split.js';
+
+export interface AppOptions {
+  container: HTMLDivElement;
+  toolbar: HTMLDivElement;
+  mainSplitContainer: HTMLDivElement;
+  bodySplitContainer: HTMLDivElement;
+  bodyLeft: HTMLDivElement;
+  bodyRight: HTMLDivElement;
+  bottomSplitContainer: HTMLDivElement;
+  bottomLeft: HTMLDivElement;
+  bottomCenter: HTMLDivElement;
+  bottomRight: HTMLDivElement;
+
+  mainSplit: Split.Instance;
+  bodySplit: Split.Instance;
+  bottomSplit: Split.Instance;
+}
+
 export class App {
+  constructor(private options: AppOptions) {
+    // Noop
+  }
+
   async init() {
-    console.log('hello from app');
-    // Do the init stuff
+    // Init flow
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,60 @@
  * The real application js file (app) will be load async.
  */
 
+import Split from 'split.js';
+
 async function main() {
+  // Get elements from the dom
+  const els = {
+    container: document.getElementById('app') as HTMLDivElement,
+    toolbar: document.getElementById('toolbar') as HTMLDivElement,
+    mainSplitContainer: document.getElementById(
+      'main-split-container'
+    ) as HTMLDivElement,
+    bodySplitContainer: document.getElementById(
+      'body-split-container'
+    ) as HTMLDivElement,
+    bodyLeft: document.getElementById('body-left') as HTMLDivElement,
+    bodyRight: document.getElementById('body-right') as HTMLDivElement,
+    bottomSplitContainer: document.getElementById(
+      'bottom-split-container'
+    ) as HTMLDivElement,
+    bottomLeft: document.getElementById('bottom-left') as HTMLDivElement,
+    bottomCenter: document.getElementById('bottom-center') as HTMLDivElement,
+    bottomRight: document.getElementById('bottom-right') as HTMLDivElement
+  };
+
+  // Init split.js
+  // We do this here, because layouting stuff is important for initial render
+  const bottomSplitContainerHeightInPercentage =
+    (200 / els.mainSplitContainer.offsetHeight) * 100;
+  const mainSplit = Split([els.bodySplitContainer, els.bottomSplitContainer], {
+    direction: 'vertical',
+    sizes: [
+      100 - bottomSplitContainerHeightInPercentage,
+      bottomSplitContainerHeightInPercentage
+    ],
+    minSize: [200, 0],
+    gutterSize: 2
+  });
+  const bodySplit = Split([els.bodyLeft, els.bodyRight], {
+    sizes: [0, 100],
+    minSize: [0, 400],
+    gutterSize: 2
+  });
+  const bottomSplit = Split(
+    [els.bottomLeft, els.bottomCenter, els.bottomRight],
+    { sizes: [25, 25, 50], gutterSize: 2 }
+  );
+
+  // Lazy-load app and init it
   const { App } = await import(/* webpackChunkName: "app" */ './app');
-  const app = new App();
+  const app = new App({
+    ...els,
+    mainSplit,
+    bodySplit,
+    bottomSplit
+  });
   await app.init();
 }
 
