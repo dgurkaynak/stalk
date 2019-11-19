460daa23d0657886fb141720972b2e55cd2bb9ef
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-11-19 09:50:33 +0300

    refactor(*): Move electron main.js to root

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function createWindow() {
   const startUrl =
     process.env.ELECTRON_START_URL ||
     url.format({
-      pathname: path.join(__dirname, '/../build/index.html'),
+      pathname: path.join(__dirname, './build/index.html'),
       protocol: 'file:',
       slashes: true
     });
