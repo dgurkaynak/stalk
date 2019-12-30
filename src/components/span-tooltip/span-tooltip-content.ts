97948dfdb8e71bb350dbb2a048a563843f5f89fc
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-12-30 21:54:18 +0300

    feat(span-tooltip): Show service, tags & process tags

--- src/components/span-tooltip/span-tooltip-content.ts
+++ src/components/span-tooltip/span-tooltip-content.ts
@@ -1,10 +1,9 @@
 import isNumber from 'lodash/isNumber';
 import SpanView from '../timeline/span-view';
-import vc from '../timeline/view-constants';
 import prettyMilliseconds from 'pretty-ms';
-import { Span } from '../../model/interfaces';
 import Axis from '../timeline/axis';
 import Stage from '../../model/stage';
+import { serviceNameOf } from '../../model/span-grouping/service-name';
 
 import './span-tooltip.css';
 import CommentSvgText from '!!raw-loader!@mdi/svg/svg/comment-outline.svg';
@@ -13,25 +12,41 @@ import TagSvgText from '!!raw-loader!@mdi/svg/svg/tag-outline.svg';
 export interface SpanTooltipContentOptions {
   axis: Axis;
   spanView?: SpanView;
+  showServiceName?: boolean;
   showLogs?: boolean;
+  spanTagsToShow?: string[];
+  processTagsToShow?: string[];
 }
 
 export class SpanTooltipContent {
+  private options: SpanTooltipContentOptions;
   private elements = {
     container: document.createElement('div'),
     header: document.createElement('div'),
     headerTime: document.createElement('span'),
     headerOperationName: document.createElement('span'),
     headerRight: document.createElement('div'),
+    serviceNameContainer: document.createElement('div'),
+    serviceName: document.createElement('span'),
     tags: document.createElement('div'),
+    processTags: document.createElement('div'),
     logs: document.createElement('div')
   };
   private stage = Stage.getSingleton();
 
-  constructor(private options: SpanTooltipContentOptions) {
+  constructor(options: SpanTooltipContentOptions) {
+    this.options = Object.assign(
+      {
+        showLogs: true,
+        showServiceName: false,
+        spanTagsToShow: [],
+        processTagsToShow: []
+      },
+      options
+    );
     const els = this.elements;
 
-    els.header.classList.add('span-tooltip-header', 'span-tooltip-section');
+    els.header.classList.add('span-tooltip-header');
     els.container.appendChild(els.header);
 
     const headerLeft = document.createElement('div');
@@ -45,11 +60,32 @@ export class SpanTooltipContent {
     els.headerOperationName.classList.add('span-tooltip-operation-name');
     headerLeft.appendChild(els.headerOperationName);
 
-    els.tags.classList.add('span-tooltip-tags', 'span-tooltip-section');
+    els.serviceNameContainer.classList.add(
+      'span-tooltip-tag',
+      'span-service-name-container',
+      'span-tooltip-border-top'
+    );
+    els.serviceNameContainer.style.display = 'none';
+    els.container.appendChild(els.serviceNameContainer);
+    const serviceNameLeft = document.createElement('span');
+    serviceNameLeft.classList.add('tag-path');
+    serviceNameLeft.textContent = 'Service:';
+    els.serviceNameContainer.appendChild(serviceNameLeft);
+    els.serviceName.classList.add('tag-value');
+    els.serviceNameContainer.appendChild(els.serviceName);
+
+    els.tags.classList.add('span-tooltip-tags', 'span-tooltip-border-top');
     els.tags.style.display = 'none';
     els.container.appendChild(els.tags);
 
-    els.logs.classList.add('span-tooltip-logs', 'span-tooltip-section');
+    els.processTags.classList.add(
+      'span-tooltip-process-tags',
+      'span-tooltip-border-top'
+    );
+    els.processTags.style.display = 'none';
+    els.container.appendChild(els.processTags);
+
+    els.logs.classList.add('span-tooltip-logs', 'span-tooltip-border-top');
     els.logs.style.display = 'none';
     els.container.appendChild(els.logs);
   }
@@ -91,7 +127,68 @@ export class SpanTooltipContent {
       `<span class="span-tooltip-log-count">${span.logs.length}</span>` +
       `<span class="span-tooltip-log-icon">${CommentSvgText}</span>`;
 
-    // TODO: Handle tags
+    // Handle service name
+    const serviceName = serviceNameOf(span);
+    if (this.options.showServiceName && serviceName) {
+      els.serviceName.textContent = serviceName;
+      els.serviceNameContainer.style.display = '';
+    } else {
+      els.serviceName.textContent = '';
+      els.serviceNameContainer.style.display = 'none';
+    }
+
+    // Handle tags
+    this.setupTags();
+    this.setupProcessTags();
+  }
+
+  private setupTags() {
+    if (!this.options.spanView) return false;
+    const span = this.options.spanView.span;
+    const els = this.elements;
+    els.tags.innerHTML = '<div class="section-header">Tags</div>';
+
+    // const tagKeysSorted = this.options.showTags.sort();
+    const tagKeysSorted = Object.keys(span.tags).sort();
+    let displayedTagCount = 0;
+    tagKeysSorted.forEach(tagKey => {
+      const value = span.tags[tagKey];
+      if (!value) return;
+      const tagEl = document.createElement('div');
+      tagEl.classList.add('span-tooltip-tag', 'margin-left');
+      tagEl.innerHTML =
+        `<span class="tag-path">${tagKey}:</span>` +
+        `<span class="tag-value">${value}</span>`;
+      els.tags.appendChild(tagEl);
+      displayedTagCount++;
+    });
+    els.tags.style.display = displayedTagCount == 0 ? 'none' : 'block';
+  }
+
+  private setupProcessTags() {
+    if (!this.options.spanView) return false;
+    const span = this.options.spanView.span;
+    const els = this.elements;
+    els.processTags.innerHTML =
+      '<div class="section-header">Process Tags</div>';
+
+    // const tagKeysSorted = this.options.processTagsToShow.sort();
+    const tagKeysSorted = Object.keys(
+      span.process ? span.process.tags || {} : {}
+    ).sort();
+    let displayedTagCount = 0;
+    tagKeysSorted.forEach(tagKey => {
+      const value = span.process.tags[tagKey];
+      if (!value) return;
+      const tagEl = document.createElement('div');
+      tagEl.classList.add('span-tooltip-tag', 'margin-left');
+      tagEl.innerHTML =
+        `<span class="tag-path">${tagKey}</span>` +
+        `<span class="tag-value">${value}</span>`;
+      els.processTags.appendChild(tagEl);
+      displayedTagCount++;
+    });
+    els.processTags.style.display = displayedTagCount == 0 ? 'none' : 'block';
   }
 
   updateMousePos(mouseX: number, mouseY: number) {
--- src/model/span-grouping/service-name.ts
+++ src/model/span-grouping/service-name.ts
@@ -6,9 +6,14 @@ export default <SpanGroupingOptions>{
   key: 'service-name',
   name: 'Service',
   groupBy: (span: Span, trace: Trace) => {
-    let serviceName = 'unknown';
-    if (span.process) serviceName = span.process.serviceName;
-    if (span.localEndpoint) serviceName = span.localEndpoint.serviceName;
+    let serviceName = serviceNameOf(span) || 'unknown';
     return [serviceName, serviceName];
   }
 };
+
+export function serviceNameOf(span: Span) {
+  let serviceName: string;
+  if (span.process) serviceName = span.process.serviceName;
+  if (span.localEndpoint) serviceName = span.localEndpoint.serviceName;
+  return serviceName;
+}
