b4cd549f0ec466f78f64f3fa1755d09a0d82b929
Deniz Gurkaynak <dgurkaynak@gmail.com>
2020-01-18 09:12:31 +0300

    fix(span-view): Fix not rendering border

--- src/components/timeline/span-view.ts
+++ src/components/timeline/span-view.ts
@@ -164,7 +164,7 @@ export default class SpanView {
       strokeWidth = 0;
       strokeColor = 'transparent';
     } else if (style === 'selected') {
-      strokeWidth = 2;
+      strokeWidth = 2.00001; // Chrome does not render borders when it's exactly 2px ?!?!
       strokeColor = this.viewPropertiesCache.borderColor;
     }
 
